e28a15aa3035de32f7f947b4d4d7c91af0750e6a
hussein-khamis <hussein-khamis@hotmail.com>
2022-07-02 12:49:00 +0300

    trying again

--- src/permissions/permission.module.ts
+++ src/permissions/permission.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
+import { CommitteesServices } from 'src/committee/committees.service';
 import { PostgresModule } from 'src/postgres/postgres.module';
+import { RolesServices } from 'src/roles/roles.service';
+import { UsersServices } from 'src/user/users.services';
 import { PermissionController } from './permission.controller';
 import { PermissionRepository } from './permission.repository';
 import { PermissionServices } from './permission.services';
 
 @Module({
-  imports: [PostgresModule],
+  imports: [PostgresModule, CommitteesServices, RolesServices, UsersServices],
   controllers: [PermissionController],
   providers: [PermissionRepository, PermissionServices],
 })
