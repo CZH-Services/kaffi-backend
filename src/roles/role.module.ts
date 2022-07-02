569db8c40df4aa4cd8cf83655ed69a30c38f2fa2
hussein-khamis <hussein-khamis@hotmail.com>
2022-07-02 19:08:52 +0300

    .

--- src/roles/role.module.ts
+++ src/roles/role.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PostgresModule } from 'src/postgres/postgres.module';
 import { RolesController } from './roles.controller';
-import { RolesRepository } from './roles.repository';
 import { RolesServices } from './roles.service';
 
 @Module({
   imports: [PostgresModule],
   controllers: [RolesController],
-  providers: [RolesRepository, RolesServices],
+  providers: [RolesServices],
   exports: [RolesServices],
 })
 export class RoleModule {}
